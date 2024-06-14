5adbdfa89f5a16b088b60eebda7c7002a53f0447
fabiosousasi <fabiosousa_si@hotmail.com>
2024-06-14 17:43:05 -0300

    Adiciona service worker

---
+++ js/service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.css',
+  '/script.js',
+  '/assets'
+  // Adicione aqui os outros arquivos que você quer cachear
+];
+
+// Instala o Service Worker e adiciona recursos ao cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Recupera recursos do cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Atualiza o Service Worker e remove caches antigos
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
