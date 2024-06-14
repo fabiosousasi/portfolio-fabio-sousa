54575a7c0891cb0bb7c326132be4f74b50e0e0c7
fabiosousasi <fabiosousa_si@hotmail.com>
2024-06-13 21:02:13 -0300

    colocando menu hamburguer

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var abrirMenu = document.querySelector(".abrir-menu-icon");
+var fecharMenu = document.querySelector(".fechar-menu-icon");
+var menuAtivo =document.querySelector(".menu-hamburger");
+
+abrirMenu.addEventListener('click', function(){
+    if(abrirMenu.style.display="block"){
+        abrirMenu.style.display="none";
+        fecharMenu.style.display="block";
+    }
+    menuAtivo.classList.add("menu-hamburger-ativo");
+});
+
+fecharMenu.addEventListener('click', function(){
+    if(abrirMenu.style.display="none"){
+        fecharMenu.style.display="none";
+        abrirMenu.style.display="block";
+    }
+    menuAtivo.classList.remove("menu-hamburger-ativo");
+});
