69a27dd18c3a342d2bf54cd7d90f504171ea5a95
fabiosousasi <fabiosousa_si@hotmail.com>
2024-06-13 21:17:07 -0300

    colocando menu hamburguer

--- js/script.js
+++ js/script.js
@@ -2,18 +2,14 @@ var abrirMenu = document.querySelector(".abrir-menu-icon");
 var fecharMenu = document.querySelector(".fechar-menu-icon");
 var menuAtivo =document.querySelector(".menu-hamburger");
 
-abrirMenu.addEventListener('click', function(){
-    if(abrirMenu.style.display="block"){
-        abrirMenu.style.display="none";
-        fecharMenu.style.display="block";
-    }
+abrirMenu.addEventListener('click', function() {
+    abrirMenu.style.display = "none";
+    fecharMenu.style.display = "block";
     menuAtivo.classList.add("menu-hamburger-ativo");
 });
 
-fecharMenu.addEventListener('click', function(){
-    if(abrirMenu.style.display="none"){
-        fecharMenu.style.display="none";
-        abrirMenu.style.display="block";
-    }
+fecharMenu.addEventListener('click', function() {
+    fecharMenu.style.display = "none";
+    abrirMenu.style.display = "block";
     menuAtivo.classList.remove("menu-hamburger-ativo");
 });
