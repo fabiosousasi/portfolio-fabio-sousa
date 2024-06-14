8aa1b1b9be3a0f0714844216ca95c08c570205cc
fabiosousasi <fabiosousa_si@hotmail.com>
2024-06-14 12:29:04 -0300

    Sessao Projetos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 var abrirMenu = document.querySelector(".abrir-menu-icon");
 var fecharMenu = document.querySelector(".fechar-menu-icon");
-var menuAtivo =document.querySelector(".menu-hamburger");
+var menuAtivo = document.querySelector(".menu-hamburger");
 
-abrirMenu.addEventListener('click', function() {
+abrirMenu.addEventListener('click', function () {
     abrirMenu.style.display = "none";
     fecharMenu.style.display = "block";
     menuAtivo.classList.add("menu-hamburger-ativo");
 });
 
-fecharMenu.addEventListener('click', function() {
+fecharMenu.addEventListener('click', function () {
     fecharMenu.style.display = "none";
     abrirMenu.style.display = "block";
     menuAtivo.classList.remove("menu-hamburger-ativo");
+});
+
+var gridPortfolio = document.querySelectorAll(".grid-portfolio");
+var gridLinkProjetos = document.querySelectorAll(".grid-portfolio-links-projetos");
+
+gridPortfolio.forEach(function(gridPortfolioAtual, index){
+    gridPortfolioAtual.addEventListener('mouseover', function(){
+        gridLinkProjetos[index].classList.add("grid-portfolio-links-projetos-ativo");
+    });
+
+    gridPortfolioAtual.addEventListener('mouseout', function(){
+        gridLinkProjetos[index].classList.remove("grid-portfolio-links-projetos-ativo");
+    });
 });
